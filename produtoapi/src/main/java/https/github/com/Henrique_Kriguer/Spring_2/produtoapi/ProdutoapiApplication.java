05f6080c457579f889c001e8dbe85dd776570dfc
Henrique-Kriguer <henriquekriguer@gmail.com>
2025-07-21 10:43:59 -0300

    generando projeto spring

---
+++ produtoapi/src/main/java/https/github/com/Henrique_Kriguer/Spring_2/produtoapi/ProdutoapiApplication.java
@@ -0,0 +1,13 @@
+package https.github.com.Henrique_Kriguer.Spring_2.produtoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoapiApplication.class, args);
+	}
+
+}
---
+++ produtoapi/src/test/java/https/github/com/Henrique_Kriguer/Spring_2/produtoapi/ProdutoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package https.github.com.Henrique_Kriguer.Spring_2.produtoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
