733584b0f988145345b52d82b15873fbae7baf49
Henrique-Kriguer <henriquekriguer@gmail.com>
2025-07-21 14:44:51 -0300

    hello-world Spring Boot

--- produtoapi/src/main/java/https/github/com/Henrique_Kriguer/Spring_2/produtoapi/ProdutoapiApplication.java
+++ produtoapi/src/main/java/https/github/com/Henrique_Kriguer/Spring_2/produtoapi/ProdutoapiApplication.java
@@ -2,12 +2,18 @@ package https.github.com.Henrique_Kriguer.Spring_2.produtoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProdutoapiApplication {
 
-	public static void main(String[] args) {
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		return "Hello World, I´m using Spring Boot";
+	}
+	public static void main(String[] args){
 		SpringApplication.run(ProdutoapiApplication.class, args);
 	}
-
 }
